b600fbf8ddc0d54f7e9829c50e8e51365feea11d
Cai Monfarrell <caimonfarrell1@gmail.com>
2021-07-29 23:07:36 -0300

    Avances de proyecto final

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(window).on('load', function() {
 });
 
 function ValidarFormulario(e) {
-	e.preventDefault();
-	let fecha = new Date();
+    e.preventDefault();
+    let fecha = new Date();
     alert("Consulta enviada el día " + fecha);
 }
 
@@ -22,6 +22,8 @@ $("input").change(function(e) {
     console.log("Valor ingresado en la caja input " + e.target.type + ": " + e.target.value);
 });
 
+
+
 /* Selectores */
 const listaProductos = document.querySelector('#lista-productos');
 const tableCarrito = document.querySelector("#lista-carrito tbody");
@@ -29,11 +31,8 @@ const btnVaciarCarrito = document.querySelector('#vaciar-carrito');
 let carrito = [];
 
 /* Listeners */
-$('#lista-productos').on('click', agregarProducto);
-$('#lista-carrito tbody').click(borrarProducto);
-
-// listaProductos.addEventListener('click', agregarProducto);
-// tableCarrito.addEventListener('click', borrarProducto);
+listaProductos.addEventListener('click', agregarProducto);
+tableCarrito.addEventListener('click', borrarProducto);
 btnVaciarCarrito.addEventListener('click', vaciarCarrito);
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -148,3 +147,43 @@ function saludar(){
 }
 
 saludar ()
+
+// Practicando algunas animaciones.... 
+
+// Numero 1 //
+// $("#slideImagen").click(() => {
+//     $("#title").slideDown("slow");
+// });
+
+// $("#slideImagenUp").click(() => {
+//     $("#title").slideUp("slow");
+// });
+
+// Numero 2 //
+// $("#muestraOculta").click(() => {
+//     $("#title").fadeToggle(1000, function() {
+//         if ($("#muestraOculta").html() == "Ocultar Titulo con FadeToggle") {
+//             $("#muestraOculta").html("Mostrar Titulo con FadeToggle");
+//         } else {
+//             $("#muestraOculta").html("Ocultar Titulo con FadeToggle");
+//         }
+//     });
+// });
+
+// ESTO YA SERÍA PARTE DEL DESAFÍO COMPLEMENTARIO // Para este ejemplo hay que actviar el título bloqueado en el html !!!!!!!!!!!!!!!!!!!1
+// $('#lista-productos').animate({
+//     // filter: brightness(0.2), (no se por que no me toma el filtro)
+//     opacity: 0.25,
+//     height: "toggle",
+//     // color: "orange", (no se por que no me toma el color de las letras....)
+// },  3000)
+
+// Para este ejemplo hay que dejar bloqueado el titulo que se encuentra en el html !!!!!!!!!!!!!!!!1111
+$(".contenedor-padding").prepend('<div id= "title" class="contenedor-pagina display-none"><h1 class="nombre-empresa">Un Vinito</h1><br><h2 class="nombre-empresa">Tu vinoteca Online</h2></div>');
+//Declaración de métodos encadenados
+$("#title").css("color", "red")
+        .slideUp(2000)
+        .delay(2000)
+        .slideDown(2000);
+
+
